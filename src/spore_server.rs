2f55ec23f2702b68ea56086bd0e92c14c79fbe2c
MateusF03 <ferrarijuliomateus@gmail.com>
2026-02-04 22:20:32 -0300

    Add Asset and AssetType structs for better asset management

--- src/spore_server.rs
+++ src/spore_server.rs
@@ -4,16 +4,45 @@ use std::fs::File;
 use std::io::copy;
 use std::path::Path;
 
+#[derive(Debug)]
+pub struct Asset {
+    pub id: i64,
+    pub asset_type: AssetType,
+}
+
+#[derive(Debug)]
+pub enum AssetType {
+    Creature,
+    Vehicle,
+    Building,
+    Ufo,
+    Adventure,
+    Unknown,
+}
+
+impl AssetType {
+    pub fn dir_name(&self) -> &'static str {
+        match self {
+            AssetType::Creature => "creatures",
+            AssetType::Vehicle => "vehicles",
+            AssetType::Building => "buildings",
+            AssetType::Ufo => "ufo",
+            AssetType::Adventure => "adventures",
+            AssetType::Unknown => "unknown",
+        }
+    }
+}
+
 pub struct SporeServer {
     endpoint: String,
-    client: Client
+    client: Client,
 }
 
 impl SporeServer {
     pub fn new() -> Self {
         Self {
             endpoint: "http://www.spore.com".to_string(),
-            client: Client::new()
+            client: Client::new(),
         }
     }
 
@@ -22,7 +51,10 @@ impl SporeServer {
     }
 
     pub fn get_sporecast_feed(&self, sporecast_id: i64) -> Result<String> {
-        self.get_text(&format!("{}/atom/sporecast/{}", self.endpoint, sporecast_id))
+        self.get_text(&format!(
+            "{}/atom/sporecast/{}",
+            self.endpoint, sporecast_id
+        ))
     }
 
     fn get_text(&self, url: &str) -> Result<String> {
@@ -42,15 +74,10 @@ impl SporeServer {
             self.endpoint, sub1, sub2, sub3, id
         );
 
-
-        let mut response = self.client
-            .get(&url)
-            .send()?
-            .error_for_status()?;
+        let mut response = self.client.get(&url).send()?.error_for_status()?;
 
         let mut file = File::create(file_path)?;
         copy(&mut response, &mut file)?;
         Ok(())
     }
 }
-
